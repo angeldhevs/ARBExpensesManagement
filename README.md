4598e380838a14db860bb7066c876e2bff287ff9
theejay <terjohnaldrixtorbela@yahoo.com>
2020-04-29 16:27:02 +0800

    firstcommit

---
+++ README.md
@@ -0,0 +1,15 @@
+# expensemanager
+
+Database name: expensemanager
+
+Run the following command:
+  - npm install
+  - composer install
+  - php artisan migrate
+  - php artisan db:seed --class=DatabaseSeeder
+
+
+
+Admin credentials
+username: admin@gmail.com
+password: admin123
---
+++ resources/js/components/common/SideBarComponent.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="sidenav">
+    <div class="profile-box">
+        <span>{{ auth.name }}({{ rolename }})</span>
+    </div>
+    <hr>
+    <ul>
+        <li>
+            <a href="/account">Account</a>
+        </li>
+        <hr>
+        <li>
+        <a href="/dashboard">Dashboard</a>
+        </li>
+        <hr>
+        <li v-if="auth.role_id == 1">
+            <a href="#">User Management</a>
+            <ul>
+                <li><a href="/role">Roles</a></li>
+                <li><a href="/user">Users</a></li>
+            </ul>
+        </li>
+        <hr>
+        <li>
+            <a href="#">Expense Management</a>
+            <ul>
+                <li  v-if="auth.role_id == 1"><a href="/expensecategory">Expense categories</a></li>
+                <li><a href="/expense">Expenses</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+    import { Request, Validate } from '../../utils/common';
+    
+    export default {
+         mounted() {
+            console.log('Sidebar mounted.')
+        },
+        props: {
+            user: String,
+            auth: Object,
+            rolename: String
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
